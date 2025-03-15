6d39845fe6f1ae04e7092b3d867ad4795dbdf5c6
bob_mervell <b0b0bm3rv3ll@gmail.com>
2025-03-15 19:28:55 +0100

    set_private variables and methods radial controller

--- RadialQuadripedController.gd
+++ RadialQuadripedController.gd
@@ -17,38 +17,38 @@ class_name RadialQuadripedController
 @export_group("Move second order")
 ## Configuration weights for move second order controller.
 ## [br]See [b]SecondOrderSystem[/b] documentation
-@export var move_second_order_config:Dictionary:
+@export var _move_second_order_config:Dictionary:
 	set(new_value):
-		move_second_order_config = new_value
-		move_second_order = SecondOrderSystem.new(move_second_order_config)
+		_move_second_order_config = new_value
+		_move_second_order = SecondOrderSystem.new(_move_second_order_config)
 ## Second order controller
-@export var move_second_order:SecondOrderSystem
+@export var _move_second_order:SecondOrderSystem
 
 @export_group("Rotate second order")
 ## Configuration weights for rotation second order controller.
 ## [br]See [b]SecondOrderSystem[/b] documentation
-@export var rotation_second_order_config:Dictionary:
+@export var _rotation_second_order_config:Dictionary:
 	set(new_value):
-		rotation_second_order_config = new_value
-		rotation_second_order = SecondOrderSystem.new(rotation_second_order_config)
+		_rotation_second_order_config = new_value
+		_rotation_second_order = SecondOrderSystem.new(_rotation_second_order_config)
 ## Second order controller
-@export var rotation_second_order:SecondOrderSystem
+@export var _rotation_second_order:SecondOrderSystem
 
 ## positions of legs end on ground
-var grounded_target_pos:Dictionary
+var _grounded_target_pos:Dictionary
 ## list of legs on ground
-var grounded_legs:Array[ThreeSegmentLeg]
+var _grounded_legs:Array[ThreeSegmentLeg]
 ## list of legs returning to resting position
-var returning_legs:Array[ThreeSegmentLeg]
+var _returning_legs:Array[ThreeSegmentLeg]
 ## list of first diagonal legs
-var diagonal_legs_1:Array[ThreeSegmentLeg]
+var _diagonal_legs_1:Array[ThreeSegmentLeg]
 ## list of second diagonal legs
-var diagonal_legs_2:Array[ThreeSegmentLeg]
+var _diagonal_legs_2:Array[ThreeSegmentLeg]
 
 ## heights of each leg (uneven or inclined terrain)
-var leg_heights:Dictionary
+var _leg_heights:Dictionary
 ## last body position
-var old_body_pos:Vector3
+var _old_body_pos:Vector3
 ## Main body velocity.
 ## [br][b][color=red]Warning:[/color][/b] Use this instead of CharacterBody3D.velocity to control character
 var body_velocity:Vector3
@@ -64,14 +64,14 @@ func _initiate_controller() -> void:
 	_initiate_body()
 	_reset_legs_orientation()
 	_initiate_legs_dictionnaries()
-	for leg:ThreeSegmentLeg in grounded_legs:
+	for leg:ThreeSegmentLeg in _grounded_legs:
 		_initiate_leg_variables(leg)
 
 func _initiate_body() -> void:
 	body.global_position = global_position
-	old_body_pos = global_position
-	move_second_order = SecondOrderSystem.new(move_second_order_config)
-	rotation_second_order = SecondOrderSystem.new(rotation_second_order_config)
+	_old_body_pos = global_position
+	_move_second_order = SecondOrderSystem.new(_move_second_order_config)
+	_rotation_second_order = SecondOrderSystem.new(_rotation_second_order_config)
 
 func _reset_legs_orientation() -> void:
 	front_left_leg.rotation.y = -PI/4
@@ -80,42 +80,42 @@ func _reset_legs_orientation() -> void:
 	hind_left_leg.rotation.y = PI/4
 
 func _initiate_legs_dictionnaries() -> void:
-	grounded_legs.append(front_left_leg)
-	grounded_legs.append(front_right_leg)
-	grounded_legs.append(hind_right_leg)
-	grounded_legs.append(hind_left_leg)
-	diagonal_legs_1.append(front_left_leg)
-	diagonal_legs_1.append(hind_right_leg)
-	diagonal_legs_2.append(front_right_leg)
-	diagonal_legs_2.append(hind_left_leg)
+	_grounded_legs.append(front_left_leg)
+	_grounded_legs.append(front_right_leg)
+	_grounded_legs.append(hind_right_leg)
+	_grounded_legs.append(hind_left_leg)
+	_diagonal_legs_1.append(front_left_leg)
+	_diagonal_legs_1.append(hind_right_leg)
+	_diagonal_legs_2.append(front_right_leg)
+	_diagonal_legs_2.append(hind_left_leg)
 
 func _initiate_leg_variables(leg:ThreeSegmentLeg) -> void:
 	leg.base.rotation.y = 0
 	leg.movement_dir = Vector3.ZERO
 	leg.leg_height = body_desired_height
-	grounded_target_pos[leg] = leg.to_global(leg.get_rest_pos())
-	leg_heights[leg] = 0
+	_grounded_target_pos[leg] = leg.to_global(leg.get_rest_pos())
+	_leg_heights[leg] = 0
 #endregion
 
 
 func _physics_process(delta: float) -> void:
 	if Engine.is_editor_hint():
-		if diagonal_legs_1.is_empty():
+		if _diagonal_legs_1.is_empty():
 			call_deferred("_initiate_controller")
 		else:
 			body_rotation = rotation
-			body_velocity = (global_position - old_body_pos)/delta
-	update_legs_variables(delta)
-	get_leg_heights()
+			body_velocity = (global_position - _old_body_pos)/delta
+	_update_legs_variables(delta)
+	_get_leg_heights()
 	move_body(delta)
 	update_body_height()
 	rotate_body(delta)
 	tilt_body()
-	old_body_pos = global_position
+	_old_body_pos = global_position
 
 #region Body movements
 func move_body(delta:float) -> void:
-	body.velocity = move_second_order.vec3_second_order_response(
+	body.velocity = _move_second_order.vec3_second_order_response(
 			delta,body_velocity,body.velocity)["output"]
 	body.velocity.y = 0
 	@warning_ignore("return_value_discarded")
@@ -124,7 +124,7 @@ func move_body(delta:float) -> void:
 func rotate_body(delta:float) -> void:
 	var desired_direction:Vector2 = Vector2.from_angle(body_rotation.y)
 	var real_direction:Vector2 = Vector2.from_angle(body.rotation.y)
-	real_direction = rotation_second_order.vec2_second_order_response(
+	real_direction = _rotation_second_order.vec2_second_order_response(
 			delta,desired_direction,real_direction)["output"]
 	body.rotation.y = real_direction.angle()
 
@@ -142,10 +142,10 @@ func update_body_height() -> void:
 		body.global_position.y = global_position.y
 
 func tilt_body() -> void:
-	var front_height:float = (leg_heights[front_left_leg] + leg_heights[front_right_leg])*.5
-	var hind_height:float = (leg_heights[hind_left_leg] + leg_heights[hind_right_leg])*.5
-	var left_height:float = (leg_heights[front_left_leg] + leg_heights[hind_left_leg])*.5
-	var right_height:float = (leg_heights[front_right_leg] + leg_heights[hind_right_leg])*.5
+	var front_height:float = (_leg_heights[front_left_leg] + _leg_heights[front_right_leg])*.5
+	var hind_height:float = (_leg_heights[hind_left_leg] + _leg_heights[hind_right_leg])*.5
+	var left_height:float = (_leg_heights[front_left_leg] + _leg_heights[hind_left_leg])*.5
+	var right_height:float = (_leg_heights[front_right_leg] + _leg_heights[hind_right_leg])*.5
 	var x_size:float = front_left_leg.to_global(front_left_leg.rest_pos).distance_to(
 		front_right_leg.to_global(front_right_leg.rest_pos))
 	var z_size:float = front_left_leg.to_global(front_left_leg.rest_pos).distance_to(
@@ -159,49 +159,49 @@ func tilt_body() -> void:
 	body.rotation.z = lerp(body.rotation.z,rotation.z,tilt_speed)
 #endregion
 
-func get_leg_heights() -> void:
-	for leg:ThreeSegmentLeg in leg_heights:
+func _get_leg_heights() -> void:
+	for leg:ThreeSegmentLeg in _leg_heights:
 		if leg.desired_state == leg.DesiredState.OK_ON_GROUND:
-			leg_heights[leg] = leg.segment_3.segment_end.global_position.y
+			_leg_heights[leg] = leg.segment_3.segment_end.global_position.y
 
 ## update legs end position and returning state
-func update_legs_variables(delta:float) -> void:
+func _update_legs_variables(delta:float) -> void:
 	var movement_dir:Vector3 = Vector3(body_velocity.x,0,body_velocity.z).normalized()#discard y movement
-	for leg:ThreeSegmentLeg in get_returning_pair():
-		returning_legs.append(leg)
+	for leg:ThreeSegmentLeg in _get_returning_pair():
+		_returning_legs.append(leg)
 		leg.is_returning = true
-	for leg:ThreeSegmentLeg in grounded_legs:
+	for leg:ThreeSegmentLeg in _grounded_legs:
 		if movement_dir != Vector3.ZERO: leg.movement_dir = movement_dir
 		@warning_ignore("unsafe_call_argument")
-		leg.target_marker.position = leg.to_local(grounded_target_pos[leg])
-	for leg:ThreeSegmentLeg in returning_legs:
+		leg.target_marker.position = leg.to_local(_grounded_target_pos[leg])
+	for leg:ThreeSegmentLeg in _returning_legs:
 		if movement_dir != Vector3.ZERO: leg.movement_dir = movement_dir
-		return_to_rest(leg,delta)
+		_return_to_rest(leg,delta)
 
 ## get leg pair which needs the most a restep
-func get_returning_pair() -> Array[ThreeSegmentLeg]:
+func _get_returning_pair() -> Array[ThreeSegmentLeg]:
 	# worst case check twice each legs (4*2 in total)
 	var anticipated_lift:bool = true
-	for leg:ThreeSegmentLeg in returning_legs:
+	for leg:ThreeSegmentLeg in _returning_legs:
 		anticipated_lift = ( anticipated_lift and
 				leg.returning_phase == leg.ReturningPhase.BACK_SWING)
 	if not anticipated_lift: return []
-	for leg:ThreeSegmentLeg in grounded_legs:
+	for leg:ThreeSegmentLeg in _grounded_legs:
 		if leg.desired_state == leg.DesiredState.MUST_RESTEP:
-			if diagonal_legs_1.has(leg): return diagonal_legs_1
-			return diagonal_legs_2
-	for leg:ThreeSegmentLeg in grounded_legs:
+			if _diagonal_legs_1.has(leg): return _diagonal_legs_1
+			return _diagonal_legs_2
+	for leg:ThreeSegmentLeg in _grounded_legs:
 		if leg.desired_state == leg.DesiredState.NEEDS_RESTEP:
-			if diagonal_legs_1.has(leg): return diagonal_legs_1
-			return diagonal_legs_2
+			if _diagonal_legs_1.has(leg): return _diagonal_legs_1
+			return _diagonal_legs_2
 	return []
 
 ## move leg target to resting_position
-func return_to_rest(leg:ThreeSegmentLeg,delta:float) -> void:
+func _return_to_rest(leg:ThreeSegmentLeg,delta:float) -> void:
 	var leg_rest:Vector3 = leg.get_rest_pos()
-	if grounded_legs.has(leg): grounded_legs.erase(leg)
+	if _grounded_legs.has(leg): _grounded_legs.erase(leg)
 	if leg.target_marker.position.distance_squared_to(leg_rest) < .0001: # is_equal_approx to sensible
-		grounded_legs.append(leg)
-		returning_legs.erase(leg)
-		grounded_target_pos[leg] = leg.to_global(leg_rest)
+		_grounded_legs.append(leg)
+		_returning_legs.erase(leg)
+		_grounded_target_pos[leg] = leg.to_global(leg_rest)
 	leg.target_marker.position = leg.get_returning_position(delta,leg.target_marker.position)
