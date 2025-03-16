9e2d6152f55dcc35bd5b8b4bd4603e37fb25ba35
bob_mervell <b0b0bm3rv3ll@gmail.com>
2025-03-16 14:39:33 +0100

    bug correction legs orientation

--- player_controller.gd
+++ player_controller.gd
@@ -7,7 +7,7 @@ func _physics_process(delta: float) -> void:
 	if not Engine.is_editor_hint():
 		var input_dir:Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 		var direction:Vector3 = ((transform.basis * Vector3(input_dir.x, 0, input_dir.y))
-				.normalized().rotated(Vector3.UP,body.global_rotation.y -3* PI/4))
+				.normalized().rotated(Vector3.UP,body.global_rotation.y + PI))
 		if direction:
 			body_velocity.x = direction.x * SPEED
 			body_velocity.z = direction.z * SPEED
